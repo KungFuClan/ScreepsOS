9fd47c85e887d34ec4285b6a905246166ae6cbaf
Brock Sharp <Uhmbrock@hotmail.com>
2021-05-18 15:49:30 -0500

    Undoing because that doesn't work

--- src/common/interfaces.ts
+++ src/common/interfaces.ts
@@ -5,21 +5,11 @@ export interface StringMap<T> {
 
 export type GeneratorCreator = (...args: any[]) => Generator;
 
-// export interface IController {
-//     processName: string,
-//     process: (Process | undefined),
-//     runMain: GeneratorCreator
-// }
-
 export abstract class IController {
-    public abstract processName: string;
-    public abstract runMain(): GeneratorCreator;
+    public static processName: string;
+    public static runMain: GeneratorCreator;
 }
 
-// export interface IService {
-//     run: GeneratorCreator
-// }
-
 export abstract class IService {
-    public abstract run(): GeneratorCreator;
+    public static run: GeneratorCreator;
 }
