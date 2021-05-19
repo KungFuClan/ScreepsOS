da3c642bc1a05ccfbf729c3d7645fcea607ba93e
Brock Sharp <Uhmbrock@hotmail.com>
2021-05-18 20:43:06 -0500

    Actual updated abstract classes implemented.

--- src/common/interfaces.ts
+++ src/common/interfaces.ts
@@ -7,9 +7,14 @@ export type GeneratorCreator = (...args: any[]) => Generator;
 
 export abstract class IController {
     public static processName: string;
-    public static runMain: GeneratorCreator;
+
+    public static runMain(): GeneratorCreator {
+        throw new Error("Method not implemented.");
+    }
 }
 
 export abstract class IService {
-    public static run: GeneratorCreator;
+    public static run(): GeneratorCreator {
+        throw new Error("Method not implemented.");
+    }
 }
