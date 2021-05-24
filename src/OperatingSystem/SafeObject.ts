2ee997908f16c4f8a05973d5b9399c22aabe1441
Jacob <jakeywaldrip@gmail.com>
2021-05-23 23:35:53 -0500

    fixed esl int

--- src/OperatingSystem/SafeObject.ts
+++ src/OperatingSystem/SafeObject.ts
@@ -1,19 +1,17 @@
-Object.defineProperty(RoomObject.prototype, "safe", {
-    get: function getSafe<T extends RoomObject>(): T {
-        const object = Game.getObjectById(this.id);
-        if(object === null) {
-            throw new ObjectNoLongerExistsError("Object no longer exists for safe access");
-        }
-        return object as T;
-    }
-});
-
 class ObjectNoLongerExistsError extends Error {
-    constructor(message: string) {
+    public constructor(message: string) {
         super(message);
         this.name = "ObjectNoLongerExistsError";
         Object.setPrototypeOf(this, new.target.prototype);
     }
 }
 
-
+Object.defineProperty(RoomObject.prototype, "safe", {
+    get: function getSafe<T extends RoomObject>(this: T): T {
+        const object = Game.getObjectById(this.id);
+        if(object === null) {
+            throw new ObjectNoLongerExistsError("Object no longer exists for safe access");
+        }
+        return object;
+    }
+});
