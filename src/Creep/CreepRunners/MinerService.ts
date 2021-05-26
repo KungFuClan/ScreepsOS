2af43633eeb991ded010eeb1cc1f55eb7f07385c
Jacob <jakeywaldrip@gmail.com>
2021-05-26 02:46:35 -0500

    fixed source targeting

--- src/Creep/CreepRunners/MinerService.ts
+++ src/Creep/CreepRunners/MinerService.ts
@@ -1,11 +1,13 @@
 import { ICreepRunner } from "Creep/interfaces/interfaces";
+import { MinerHelper } from "Creep/Helpers/MinerHelper";
 import { RoomRepo } from "Repositories/RoomRepo";
 
 export const MinerService: ICreepRunner = {
     *runRole (creepName: string): Generator {
 
         const creep = Game.creeps[creepName];
-        const sources = creep.pos.findClosestByRange(RoomRepo.GetAllSources_ByRoom(creep.room.name));
+        const sources = RoomRepo.GetAllSources_ByRoom(creep.room.name);
+        const targetSource = creep.pos.findClosestByRange(MinerHelper.GetSourceWithLowestWorkSaturation(sources, creep.room.name));
 
         yield true;
     }
