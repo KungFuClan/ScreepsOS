82025a1b6b2fa61dbfeec13e93e4237d5f4657f1
Jacob <jakeywaldrip@gmail.com>
2021-05-29 06:14:19 -0500

    got last statements i missed

--- src/Creep/CreepRunners/MinerService.ts
+++ src/Creep/CreepRunners/MinerService.ts
@@ -15,10 +15,9 @@ export const MinerService: ICreepRunner = {
     *runRole (this: Thread<IRunCreepParams>, creepName: string): Generator {
 
         const cache: StringMap<any> = {}
-        _logger.info("Cache created for miner");
 
         while(Game.creeps[creepName]) {
-            _logger.info("Running miner logic");
+
             const creep = Game.creeps[creepName];
             let targetSource = CreepRepo.GetCreepMemoryTarget<Source>(creep);
             if(!targetSource) {
